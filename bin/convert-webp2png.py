7b4afd86f2b9cb098538eb4b6c07f929b59d008b
Niko Kivelä <niko@tovrleaf.com>
2021-09-13 23:49:32 +0300

    Add Python image conversion script

---
+++ bin/convert-webp2png.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+import sys
+
+fromFile = sys.argv[1]
+toStr = 'public/certs/%s.png' % '_'.join(sys.argv[2:]).lower()
+
+im = Image.open(fromFile).convert('RGB')
+im.save(toStr, 'png')
+print(f'Created a new file:  {toStr}')
+
+os.remove(fromFile)
+print(f'Removed source file: {fromFile}')
