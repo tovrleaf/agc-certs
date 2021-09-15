7da68dca89d3f16aeceb9c2a16c3b22703a6c4c9
Niko Kivelä <niko@tovrleaf.com>
2021-09-15 23:09:30 +0300

    Add Python 3 Scripting for System Administrators

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "./App.css";
 const imgArr = {
   'introduction_to_cloud_computing': ['Introduction to Cloud Computing', '0.4', '13 Sep 2021'],
   'serverless_concepts': ['Serverless Concepts', '1.2', '13 Sep 2021'],
-  'network_routing_fundamentals': ['Network Routing Fundamentals', '2.8', '14 Sep 2021'] 
+  'network_routing_fundamentals': ['Network Routing Fundamentals', '2.8', '14 Sep 2021'],
+  'python_3_scripting_for_system_administrators': ['Python 3 Scripting for System Administrators', '10', '15 Sep 2021']
 }
 var images = [];
 for (const [k, v] of Object.entries(imgArr)) {
