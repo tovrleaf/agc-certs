8410caae8c83f9c5e021f34d0f9bccb35cdea19d
Niko Kivelä <niko@tovrleaf.com>
2021-09-24 10:00:45 +0300

    Add course Introduction to AWS CloudFormation

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const imgArr = {
   'storage_databases_and_migration_for_associate_aws_solutions_architects': ['Storage, Databases, and Migration for Associate AWS Solutions Architects', 7.8, '22 Sep 2021'],
   'introduction_to_amazon_rds':                   ['Introduction to Amazon RDS', 3.1, '23 Sep 2021'],
   'aws_certified_cloud_practitioner_2020':        ['AWS Certified Cloud Practitioner 2020', 13.2, '21 Nov 2020'],
+  'introduction_to_aws_cloudformation':           ['Introduction to AWS CloudFormation', 2, '24 Sep 2021'],
 }
 
 // sort by date
