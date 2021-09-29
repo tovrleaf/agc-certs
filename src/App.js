1c5016e0a82339be711d9f7c7b62f838068d06af
Niko Kivelä <niko@tovrleaf.com>
2021-09-29 20:37:06 +0300

    Move course to a separate file

--- src/App.js
+++ src/App.js
@@ -2,30 +2,10 @@ import React, { Component } from "react";
 import Gallery from 'react-grid-gallery';
 import "./App.css";
 import transform from './nameTransform';
-
-const imgArr = [
-  ['Introduction to Cloud Computing', 0.4, '13 Sep 2021'],
-  ['Serverless Concepts', 1.2, '13 Sep 2021'],
-  ['Network Routing Fundamentals', 2.8, '14 Sep 2021'],
-  ['Python 3 Scripting for System Administrators', 10, '15 Sep 2021'],
-  ['AWS Certification Preparation Guide', 3.2, '16 Sep 2021'],
-  ['Python for Beginners (legacy)', 6.1, '17 Sep 2021'],
-  ['Coding for Cloud 101 (legacy)', 1.4, '18 Sep 2021'],
-  ['AWS Cost Control (legacy)', 4.5, '19 Sep 2021'],
-  ['AWS Cloud Services and Infrastructure - Cost Optimization Deep Dive', 11.7, '20 Sep 2021'],
-  ['Introduction to AWS (Legacy)', 1.5, '21 Sep 2021'],
-  ['Storage, Databases, and Migration for Associate AWS Solutions Architects', 7.8, '22 Sep 2021'],
-  ['Introduction to Amazon RDS', 3.1, '23 Sep 2021'],
-  ['AWS Certified Cloud Practitioner 2020', 13.2, '21 Nov 2020'],
-  ['Introduction to AWS CloudFormation', 2, '24 Sep 2021'],
-  ['Kubernetes Deep Dive', 4.7, '26 Sep 2021'],
-  ['S3 Masterclass', 12.1, '27 Sep 2021'],
-  ['AWS Essentials', 14.9, '28 Sep 2021'],
-  ['Designing Resilient Architectures for Associate AWS Solutions Architects', 5, '29 Sep 2021'],
-]
+import courses from './courses'
 
 // sort by date
-var sorted = Object.entries(imgArr).sort(([, a] ,[, b]) => Date.parse(a[2]) - Date.parse(b[2])).reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+var sorted = Object.entries(courses()).sort(([, a] ,[, b]) => Date.parse(a[2]) - Date.parse(b[2])).reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
 
 var images = [];
 var total = 0;
