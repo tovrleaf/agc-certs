3b098a664b8437450fb10eecf5ce7ee613e36da5
Niko Kivelä <niko@tovrleaf.com>
2021-09-29 20:29:28 +0300

    Use scipt to generate file names

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
 import Gallery from 'react-grid-gallery';
 import "./App.css";
+import transform from './nameTransform';
 
-const imgArr = {
-  'introduction_to_cloud_computing':              ['Introduction to Cloud Computing', 0.4, '13 Sep 2021'],
-  'serverless_concepts':                          ['Serverless Concepts', 1.2, '13 Sep 2021'],
-  'network_routing_fundamentals':                 ['Network Routing Fundamentals', 2.8, '14 Sep 2021'],
-  'python_3_scripting_for_system_administrators': ['Python 3 Scripting for System Administrators', 10, '15 Sep 2021'],
-  'aws_certification_preparation_guide':          ['AWS Certification Preparation Guide', 3.2, '16 Sep 2021'],
-  'python_for_beginner_legacy':                   ['Python for Beginners (legacy)', 6.1, '17 Sep 2021'],
-  'coding_for_cloud_101_legacy':                  ['Coding for Cloud 101 (legacy)', 1.4, '18 Sep 2021'],
-  'aws_cost_control_legacy':                      ['AWS Cost Control (legacy)', 4.5, '19 Sep 2021'],
-  'aws_cloud_services_and_infrastructure_-_cost_optimization_deep_dive': ['AWS Cloud Services and Infrastructure - Cost Optimization Deep Dive', 11.7, '20 Sep 2021'],
-  'introduction_to_aws_legacy':                   ['Introduction to AWS (Legacy)', 1.5, '21 Sep 2021'],
-  'storage_databases_and_migration_for_associate_aws_solutions_architects': ['Storage, Databases, and Migration for Associate AWS Solutions Architects', 7.8, '22 Sep 2021'],
-  'introduction_to_amazon_rds':                   ['Introduction to Amazon RDS', 3.1, '23 Sep 2021'],
-  'aws_certified_cloud_practitioner_2020':        ['AWS Certified Cloud Practitioner 2020', 13.2, '21 Nov 2020'],
-  'introduction_to_aws_cloudformation':           ['Introduction to AWS CloudFormation', 2, '24 Sep 2021'],
-  'kubernetes_deep_dive':                         ['Kubernetes Deep Dive ', 4.7, '26 Sep 2021'],
-  's3_masterclass':                               ['S3 Masterclass ', 12.1, '27 Sep 2021'],
-  'aws_essentials':                               ['AWS Essentials ', 14.9, '28 Sep 2021'],
-  'designing_resilient_architectures_for_associate_aws_solutions_architects': ['Designing Resilient Architectures for Associate AWS Solutions Architects', 5, '29 Sep 2021'],
-}
+const imgArr = [
+  ['Introduction to Cloud Computing', 0.4, '13 Sep 2021'],
+  ['Serverless Concepts', 1.2, '13 Sep 2021'],
+  ['Network Routing Fundamentals', 2.8, '14 Sep 2021'],
+  ['Python 3 Scripting for System Administrators', 10, '15 Sep 2021'],
+  ['AWS Certification Preparation Guide', 3.2, '16 Sep 2021'],
+  ['Python for Beginners (legacy)', 6.1, '17 Sep 2021'],
+  ['Coding for Cloud 101 (legacy)', 1.4, '18 Sep 2021'],
+  ['AWS Cost Control (legacy)', 4.5, '19 Sep 2021'],
+  ['AWS Cloud Services and Infrastructure - Cost Optimization Deep Dive', 11.7, '20 Sep 2021'],
+  ['Introduction to AWS (Legacy)', 1.5, '21 Sep 2021'],
+  ['Storage, Databases, and Migration for Associate AWS Solutions Architects', 7.8, '22 Sep 2021'],
+  ['Introduction to Amazon RDS', 3.1, '23 Sep 2021'],
+  ['AWS Certified Cloud Practitioner 2020', 13.2, '21 Nov 2020'],
+  ['Introduction to AWS CloudFormation', 2, '24 Sep 2021'],
+  ['Kubernetes Deep Dive', 4.7, '26 Sep 2021'],
+  ['S3 Masterclass', 12.1, '27 Sep 2021'],
+  ['AWS Essentials', 14.9, '28 Sep 2021'],
+  ['Designing Resilient Architectures for Associate AWS Solutions Architects', 5, '29 Sep 2021'],
+]
 
 // sort by date
 var sorted = Object.entries(imgArr).sort(([, a] ,[, b]) => Date.parse(a[2]) - Date.parse(b[2])).reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
@@ -29,9 +30,10 @@ var sorted = Object.entries(imgArr).sort(([, a] ,[, b]) => Date.parse(a[2]) - Da
 var images = [];
 var total = 0;
 for (const [k, v] of Object.entries(sorted).reverse()) {
+  const pic = transform.transform(v[0])
   const obj = {
-    src: CERTS_DIR + `certs/${k}.png`,
-    thumbnail: CERTS_DIR + `certs/${k}.png`,
+    src: CERTS_DIR + `certs/${pic}`,
+    thumbnail: CERTS_DIR + `certs/${pic}`,
     thumbnailWidth: 240,
     thumbnailHeight: 185,
     tags: [{value: `${v[1]} hours`, title: "Duration"}, {value: v[2], title: "Completion"}],
--- src/nameTransform.js
+++ src/nameTransform.js
@@ -1,2 +1,15 @@
-const myArgs = process.argv.slice(2);
-console.log(myArgs.join(' ').toLowerCase().replace(/[^a-z0-9-_ ]/g, '').replace(/ /g, '_') + '.png')
+var isCLI = require.main === module;
+
+const transform = (str) => {
+  if (typeof str === 'string') {
+    str = str.split(' ');
+  }
+  return str.join(' ').toLowerCase().replace(/[^a-z0-9-_ ]/g, '').replace(/ /g, '_') + '.png'
+}
+
+if (isCLI) {
+  const myArgs = process.argv.slice(2);
+  console.log(transform(myArgs))
+}
+
+module.exports.transform = transform;
